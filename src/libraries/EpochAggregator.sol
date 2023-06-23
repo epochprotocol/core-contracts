bf075f22696316d419d5bf7a39910d9aa7e81583
abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2023-06-23 15:36:30 +0530

    WIP: aggregator

---
+++ src/libraries/EpochAggregator.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import {calldataKeccak} from "../helpers/LibraryHelpers.sol";
+
+struct UserOperation {
+    address sender;
+    uint256 nonce;
+    bytes initCode;
+    bytes callData;
+    uint256 callGasLimit;
+    uint256 verificationGasLimit;
+    uint256 preVerificationGas;
+    uint256 maxFeePerGas;
+    uint256 maxPriorityFeePerGas;
+    bytes paymasterAndData;
+    bytes signature;
+}
+
+library EpochAggregatorLibrary {
+    function getSender(
+        UserOperation calldata userOp
+    ) internal pure returns (address) {
+        address data;
+        //read sender from userOp, which is first userOp member (saves 800 gas...)
+        assembly {
+            data := calldataload(userOp)
+        }
+        return address(uint160(data));
+    }
+
+    //relayer/block builder might submit the TX with higher priorityFee, but the user should not
+    // pay above what he signed for.
+    function gasPrice(
+        UserOperation calldata userOp
+    ) internal view returns (uint256) {
+        unchecked {
+            uint256 maxFeePerGas = userOp.maxFeePerGas;
+            uint256 maxPriorityFeePerGas = userOp.maxPriorityFeePerGas;
+            if (maxFeePerGas == maxPriorityFeePerGas) {
+                //legacy mode (for networks that don't support basefee opcode)
+                return maxFeePerGas;
+            }
+            return min(maxFeePerGas, maxPriorityFeePerGas + block.basefee);
+        }
+    }
+
+    function pack(
+        UserOperation calldata userOp
+    ) internal pure returns (bytes memory ret) {
+        address sender = getSender(userOp);
+        bytes32 hashInitCode = calldataKeccak(userOp.initCode);
+        bytes32 hashCallData = calldataKeccak(userOp.callData);
+        uint256 callGasLimit = userOp.callGasLimit;
+        uint256 verificationGasLimit = userOp.verificationGasLimit;
+        uint256 preVerificationGas = userOp.preVerificationGas;
+        uint256 maxFeePerGas = userOp.maxFeePerGas;
+        uint256 maxPriorityFeePerGas = userOp.maxPriorityFeePerGas;
+        bytes32 hashPaymasterAndData = calldataKeccak(userOp.paymasterAndData);
+
+        return
+            abi.encode(
+                sender,
+                hashInitCode,
+                hashCallData,
+                callGasLimit,
+                verificationGasLimit,
+                preVerificationGas,
+                maxFeePerGas,
+                maxPriorityFeePerGas,
+                hashPaymasterAndData
+            );
+    }
+
+    function hash(
+        UserOperation calldata userOp
+    ) internal pure returns (bytes32) {
+        return keccak256(pack(userOp));
+    }
+
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+
+    // function validateSignatures() {}
+}
