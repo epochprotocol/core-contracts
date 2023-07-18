e3b3ea5918a704896a916289b275cd4e747a030a
abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2023-07-18 16:00:30 +0530

    Added Test for batch tx

--- src/test/EpochWallet.t.sol
+++ src/test/EpochWallet.t.sol
@@ -17,7 +17,14 @@ contract EpochWalletTest is Test {
         0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
     uint256 public immutable salt = 123456;
 
+    mapping(uint256 => bool) gotFallback;
+
     receive() external payable {}
+
+    fallback() external payable {
+        uint256 data = abi.decode(msg.data, (uint256));
+        gotFallback[data] = true;
+    }
 
     constructor() {
         string memory mainnetUrl = vm.envString("MAINNET_RPC_URL");
@@ -34,5 +41,16 @@ contract EpochWalletTest is Test {
         bytes memory data = new bytes(0);
         wallet.executeEpoch(1, address(this), 1 ether, data);
         assertEq(address(wallet).balance, 99 ether);
+    }
+
+    function testExecuteEpochBatch() public {
+        uint256 taskid = 1;
+        bytes memory data = abi.encode(taskid);
+        address[] memory dests = new address[](1);
+        dests[0] = address(this);
+        bytes[] memory funcs = new bytes[](1);
+        funcs[0] = data;
+        wallet.executeBatchEpoch(taskid, dests, funcs);
+        assertEq(gotFallback[taskid], true);
     }
 }
